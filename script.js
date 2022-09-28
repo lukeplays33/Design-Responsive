c5b23debe87b10b16380dfdc01342ef202df4cea
Sketch <54947833+lukeplays33@users.noreply.github.com>
2022-09-28 19:45:44 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById('t').onclick = function () {
+window.open('https://twitter.com/Sketch68862592');
+}
+
+document.getElementById('yt').onclick = function () {
+window.open('https://www.youtube.com/channel/UCdI5Lpnho7khE1DnDyrD6Ig');
+}
+
+document.getElementById('start1').onclick = function () {
+window.location.href = 'https://lukeplays33.github.io/Design-Responsive-Home/';
+}
+
+document.getElementById('start2').onclick = function () {
+window.location.href = 'https://lukeplays33.github.io/Design-Responsive-Home/';
+}
+
+document.getElementById('start3').onclick = function () {
+window.location.href = 'https://lukeplays33.github.io/Design-Responsive-Home/';
+}
