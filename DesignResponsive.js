6af6d46db6def9dd32153475a5bcb3204d4047dd
Sketch <54947833+lukeplays33@users.noreply.github.com>
2022-11-06 14:31:15 +0100

    Update DesignResponsive.js

--- DesignResponsive.js
+++ DesignResponsive.js
@@ -8,8 +8,8 @@ if(window.innerWidth < 600) {
    } else {
      const am = window.innerWidth - 1600;
      const div = am / 600;
-     width = width + 550 * div
-     height = height + 140 * div
+     width = width + 450 * div
+     height = height + 90 * div
    }
 
 var noChangeWidth = width;
@@ -40,8 +40,8 @@ if(window.innerWidth < 600) {
    } else {
      const am = window.innerWidth - 1600;
      const div = am / 600;
-     width = width + 550 * div
-     height = height + 140 * div
+     width = width + 450 * div
+     height = height + 90 * div
    }
   
   noChangeWidth = w;
