6dc149fca4cc92276c22c6898a8e40513283e583
Sketch <54947833+lukeplays33@users.noreply.github.com>
2022-11-16 10:10:26 +0100

    Update DesignResponsive.js

--- DesignResponsive.js
+++ DesignResponsive.js
@@ -107,7 +107,7 @@ id.style.position = 'static';
          
            var h = Number(String(val).replaceAll('px',''));
        var j = window.innerWidth < 600 ? height-280 : height;
-      var pc = Number(h/j) * 150;
+      var pc = Number(h/j) * 120;
           id.style[i] = String(pc) + 'vmin';
      }
   }
